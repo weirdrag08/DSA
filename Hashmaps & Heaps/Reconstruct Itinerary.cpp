97340630bd3ae9bda90427f45e55b387455cd5fb
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2025-10-07 22:43:32 +0530

    Update Reconstruct Itinerary.cpp

--- Hashmaps & Heaps/Reconstruct Itinerary.cpp
+++ Hashmaps & Heaps/Reconstruct Itinerary.cpp
@@ -7,28 +7,33 @@ using namespace std;
 
 vector<string> getItinerary(vector<vector<string> > &tickets){
     int n = tickets.size();
-    unordered_set<string> intermediates;
+    unordered_map<string, bool> cities;
     unordered_map<string, string> frq;
     for(int i = 0; i < n; i++){
         //{key, value}: whatever value appears, they can't be starting point and will act as intermediates, as they were reachable from key
         string source = tickets[i][0], destination = tickets[i][1];
         frq[source] = destination;
-        intermediates.insert(destination);
-    }
-
-    vector<string> res;
-
-    for(int i = 0; i < n; i++){
-        if(intermediates.count(tickets[i][0]) == 0){
-            string city = tickets[i][0];
-            res.push_back(city);
-            for(auto it = frq.find(city); it != frq.end();){
-                res.push_back(it-> second);
-                it = frq.find(it-> second);
-            }
-            break;
+        cities[destination] = false;
+        if(cities.count(source) == 0){
+            cities[source] = true;
         }
     }
+
+    string startCity = "";
+    for(auto city : cities){
+        if(city.second == true){
+            startCity = city.first;
+        }
+    }
+    
+    vector<string> res;
+    
+    res.push_back(startCity);
+    for(auto it = frq.find(startCity); it != frq.end();){
+        res.push_back(it-> second);
+        it = frq.find(it-> second);
+    }    
+    
     return res;
 }
 
