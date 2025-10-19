11f69655f16ea671366b824582b8af476d3bc853
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2025-10-19 19:39:08 +0530

    Minimum Window Subsequence implementation
    IMPORTANT!!!! :D

--- Hashmaps & Heaps/Minimum Window Subsequence.cpp
+++ Hashmaps & Heaps/Minimum Window Subsequence.cpp
@@ -1,35 +1,42 @@
-class Solution {
-  public:
-    int subseqPresent(int l, int r, string &s1, string &s2){
-        int k = 0;
-        for(int i = l; i <= r && k < s2.size(); i++){
-            if(s1[i] == s2[k]){
-                k++;
-            }
+#include <bits/stdc++.h>
+using namespace std;
+
+int subseqPresent(int l, int r, string &s1, string &s2){
+    int k = 0;
+    for(int i = l; i <= r && k < s2.size(); i++){
+        if(s1[i] == s2[k]){
+            k++;
         }
-        return k;
     }
-    
-    string minWindow(string &s1, string &s2){
-        int start = -1, length = INT_MAX, k = 0;
-        for(int i = 0, j = 0; i < s1.size(); i++){
-            //Add element
-            if(s1[i] == s2[k]){
-                k++;
-            }
-            //While valid, calculate answer, Make it Invalid by moving 'j'
-            while(k == s2.size()){
-                //Calculate answer
-                if(i - j + 1 < length){
-                    start = j;
-                    length = i - j + 1;
-                }
-                
-                //Remove 'j', and check if the s2 is present as a subsequence in the current segment
-                j++;
-                k = subseqPresent(j, i, s1, s2);
-            }
+    return k;
+}
+
+string minWindow(string &s1, string &s2){
+    int start = -1, length = INT_MAX, k = 0;
+    for(int i = 0, j = 0; i < s1.size(); i++){
+        //Add element
+        if(s1[i] == s2[k]){
+            k++;
         }
-        return start == -1 ? "" : s1.substr(start, length);
+        //While valid, calculate answer, Make it Invalid by moving 'j'
+        while(k == s2.size()){
+            //Calculate answer
+            if(i - j + 1 < length){
+                start = j;
+                length = i - j + 1;
+            }
+            
+            //Remove 'j', and check if the s2 is present as a subsequence in the current segment
+            j++;
+            k = subseqPresent(j, i, s1, s2);
+        }
     }
-};
+    return start == -1 ? "" : s1.substr(start, length);
+}
+
+int main() {
+	// your code goes here
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << minWindow(s1, s2) << '\n';
+}
