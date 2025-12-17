b3a0d258e74348ed6edf196877f51b487f10bbd6
Aditya Anand <60457205+weirdrag08@users.noreply.github.com>
2025-12-17 18:57:04 +0530

    Refine comments for partitioning algorithm clarity
    Updated comments for clarity and improved understanding of the partitioning logic.

--- MITM/LC - 2035 Partition Array Into Two Arrays to Minimize Sum Difference.cpp
+++ MITM/LC - 2035 Partition Array Into Two Arrays to Minimize Sum Difference.cpp
@@ -51,35 +51,41 @@ public:
             sort(p.second.begin(), p.second.end());
         }
         
-        //x + y represent TOGETHER represent a singular partition, the remaning value from each half halves is responsible for the other partition
-        //For each value (x) of unsorted half, BS on sorted half to find a value (y) which is the CLOSEST VALUE to (totalSum - x)
-        //i.e. => y ≈ (totalSum - x), which is y >= totalSum - x or y <= totalSum - x (Neighbouring values)
+        //x + y TOGETHER represent a singular partition, the remaning value from each halves is responsible for the other partition
         //S1 = x + y
         //S2 = totalSum - S1 => totalSum - (x + y)
         //OBJECTIVE: Min|S2 - S1| => Min|totalSum - (x + y) - (x + y)|
         //IMPORTANT => Min|totalSum - 2(x + y)|
         //It can be rewritten as:
-        //totalSum - 2(x + y) ≈ 0
-        //y ≈ (totalSum - 2x) / 2 (Also gives right answer)
+        //totalSum - 2(x + y) ≈ 0 (as we want to minimise the value, it should be as close as possible to 0)
+        //y ≈ (totalSum - 2x) / 2
+        //Conclusion:
+        //For each value (x) of unsorted half, BS on sorted half to find a value (y) ≈ (totalSum - 2x) / 2 {y is as close as it can be}
+        //i.e. => y ≈ (totalSum - 2x) / 2, which is y <= (totalSum - 2x) / 2 {y as Left Neighbour} or y >= (totalSum - 2x) / 2 {y as Right Neighbour}
+
 
         //Better Representation of the same equation would be:
         //x + y ≈ (totalSum / 2)
-        // y ≈ (totalSum / 2) - x (BS condition to find value of y such that it is closest to totalSum - x)
+        // y ≈ (totalSum / 2) - x {BS condition to find value of y such that it is closest to (totalSum / 2) - x}
 
         int ans = INT_MAX;
         for(auto p : left){
             int size = p.first;
+            //Subset sum formed by exactly #size elements in the left half = x
+            //Check if there are subset sums in the right half formed by exactly #[(n / 2) - size] elements
+            //If yes, adding both subsets sum will result in exactly n / 2 elements(FIRST PARTITION), their sum as x + y representing the FIRST PARTITION together (we get appropriate y from BS on right half)
             if(right.count((n / 2) - size) > 0){
+                //ForEach(x), find appropriate 'y', that will help minimise the OBJECTIVE
                 for(int i : p.second){
                     int x = i, y = -1;
                     int idx = bs((totalSum / 2) - x, right[(n / 2) - size]);
-                    //Left neighbour
+                    //Left neighbour, i.e. <= (totalSum / 2) - x
                     if(idx != -1){
                         y = right[(n / 2) - size][idx];
                         ans = min(ans, abs(totalSum - 2 * (x + y)));
                     }
                     //Right Neighbour
-                    //The other closest value would be to the right of totalSum - x, i.e. > totalSum - x, which will be at idx + 1, if it exists
+                    //The other closest value would be to the right of (totalSum / 2) - x, i.e. > (totalSum / 2) - x, which will be at idx + 1, if it exists
                     if(idx + 1 < right[(n / 2) - size].size()){
                         y = right[(n / 2) - size][idx + 1];
                         ans = min(ans, abs(totalSum - 2 * (x + y)));
